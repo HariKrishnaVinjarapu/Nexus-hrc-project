a182f18e1bc66e5ebfd508d7c437b28d8828a6f1
hari <vhari018@gmail.com>
2024-07-17 18:13:21 +0530

    first commit

--- src/main/java/com/hr/test/HrcprojectApplication.java
+++ src/main/java/com/hr/test/HrcprojectApplication.java
@@ -2,9 +2,10 @@ package com.hr.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HrcprojectApplication {
+public class HrcprojectApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrcprojectApplication.class, args);
